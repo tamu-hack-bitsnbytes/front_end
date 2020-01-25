79a3b1cdf57ba86b736fed834f1b820030e383af
Mauricio Degregori <degregori2@gmail.com>
2020-01-25 15:58:21 -0600

    added search flight form

--- air-predict/src/components/FlightSearch.jsx
+++ air-predict/src/components/FlightSearch.jsx
@@ -2,7 +2,42 @@ import React from "react";
 
 const FlightSearch = () => {
     return (
-        <><h3>FlightSearch</h3></>
+        <div>
+            <h3>FlightSearch</h3>
+            <form>
+                <div>
+                    <input
+                        type="text"
+                        name="destination"
+                        placeholder="enter destination"
+                        // value=
+                        // onChange=
+                    />
+                    <input
+                        type="text"
+                        name="departure_location"
+                        placeholder="Enter Departure Location"
+                        // value=
+                        // onChange=
+                    />
+                     <input
+                        type="date"
+                        name="departure_date"
+                        placeholder="Enter departure Date"
+                        // value=
+                        // onChange=
+                    />
+                    <input
+                        type="time"
+                        name="departure_time"
+                        placeholder="enter departure time"
+                        // value=
+                        // onChange=
+                    />
+                </div>
+                <button>Search Flights</button>
+            </form>
+        </div>
     )
 }
 
